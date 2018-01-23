0017528f64b0c4b7976a9c824d551d97228fe7ce
Fabian Lemke <info@kikkirej.net>
2018-01-23 08:29:03 +0100

    Refactoring

--- Scaler/Program.cs
+++ Scaler/Program.cs
@@ -16,11 +16,25 @@ namespace Scaler
             int newScale = 480;
             if (args.Length > 0)
             {
-                if (args[0].Equals("help")){
+                if (args[0].Equals("help"))
+                {
                     Console.WriteLine(@"https://www.tenforums.com/tutorials/5990-change-dpi-scaling-level-displays-windows-10-a.html#option4");
                     Console.WriteLine("100%: 96");
                     Console.WriteLine("125%: 120");
                     Console.WriteLine("150%: 144");
+                    Console.WriteLine("Hilfsdialog: help");
+                    Console.WriteLine("Deaktivieren: disable");
+                    Console.WriteLine("ScalerCount: count <number>");
+                    return;
+                }
+                else if (args[0].Equals("disable"))
+                {
+                    new Scaler().DisableScale();
+                    return;
+                }
+                else if (args[0].Equals("count"))
+                {
+                    Console.Write("not implemented");
                     return;
                 }
                 else
@@ -31,15 +45,13 @@ namespace Scaler
                     }
                     catch (Exception)
                     {
-                        
+
                     }
                 }
             }
             try
             {
-                RegistryKey key= Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
-                key.SetValue("LogPixels", newScale);
-                key.SetValue("Win8DpiScaling", 1);
+                new Scaler(newScale).Scale();
             }
             catch (Exception)
             {
---
+++ Scaler/Scaler.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scaler
+{
+    class Scaler
+    {
+        private readonly int ratio = 480;
+
+        public Scaler(int ratio)
+        {
+            if(this.ratio<96 || this.ratio > 480)
+            {
+                throw new Exception("Es ist nur eine Skalierung von 96 bis 480 möglich. Mehr Informationen gibt das Flag \"help\".");
+            }
+            this.ratio = ratio;
+        }
+
+        public Scaler()
+        {
+
+        }
+
+        public void Scale()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue("LogPixels", ratio);
+            key.SetValue("Win8DpiScaling", 1);
+        }
+
+        public void DisableScale()
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue("Win8DpiScaling", 0);
+        }
+    }
+}
